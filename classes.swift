1e01681549c54a5a56ffc65ec37ab589e01792a9
dragon7th <dsvnth777@gmail.com>
2025-07-29 03:24:52 +0800

    Update classes.swift

--- classes.swift
+++ classes.swift
@@ -23,15 +23,24 @@ novall.work()
 novall.printSummary()
 
 //If a child class wants to change a method from a parent class, it must use override:
-override func printSummary() {
-    print("I spend \(hours) hours a day searching Stack Overflow.")
+class Developer: Employee {
+    func work() {
+        print("I'm coding for \(hours) hours.")
+    }
+    override func printSummary() {
+    print("I've spent so much time with ai from Goose fo like \(hours)")
+}
 }
 
-//The second difference is that initializers are more tricky with classes. There’s a lot of complexity here, but there are three key points:
+//The second difference is that initializers are more tricky with classes. 
+//There’s a lot of complexity here, but there are three key points:
 
-/* Swift won’t generate a memberwise initializer for classes.
-If a child class has custom initializers, it must always call the parent’s initializer after it has finished setting up its own properties.
-If a subclass doesn’t have any initializers, it automatically inherits the initializers of its parent class.
+/* 
+1. Swift won’t generate a memberwise initializer for classes.
+2. If a child class has custom initializers, it must always call the parent’s 
+initializer after it has finished setting up its own properties.
+3. If a subclass doesn’t have any initializers, it automatically inherits the 
+initializers of its parent class.
 */
 class Vehicle {
     let isElectric: Bool
@@ -50,8 +59,15 @@ class Car: Vehicle {
     }
 }
 
-// super allows us to call up to methods that belong to our parent class, such as its initializer.
-// The third difference is that all copies of a class instance share their data, meaning that changes you make to one will automatically change other copies.
+let myCar = Car(isElectric: true, isConvertible: false)
+
+print("Is my car electric? \(myCar.isElectric)")
+print("Is my car convertible? \(myCar.isConvertible)")
+// super allows us to call up to methods that belong to our parent class, 
+// such as its initializer.
+
+// The third difference is that all copies of a class instance share their data, 
+// meaning that changes you make to one will automatically change other copies.
 class Singer {
     var name = "Adele"
 }
@@ -62,8 +78,10 @@ singer2.name = "Justin"
 print(singer1.name)  
 print(singer2.name)
 
-//That will print “Justin” for both – even though we only changed one of them, the other also changed. In comparison, struct copies don’t share their data.
-//The fourth difference is that classes can have a deinitializer that gets called when the last reference to an object is destroyed.
+//That will print “Justin” for both – even though we only changed one of them, 
+//the other also changed. In comparison, struct copies don’t share their data.
+//The fourth difference is that classes can have a deinitializer that gets called 
+//when the last reference to an object is destroyed.
 //So, we could create a class that prints a message when it’s created and destroyed:
 class User {
     let id: Int
@@ -83,7 +101,8 @@ for i in 1...3 {
     print("User \(user.id): I'm in control!")
 }
 
-//The final difference is that classes let us change variable properties even when the class itself is constant:
+//The final difference is that classes let us change variable properties 
+//even when the class itself is constant:
 class User {
     var name = "Paul"
 }
